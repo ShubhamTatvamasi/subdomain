5bfb82fd014fda7ff100ecea0ce0524c407c9096
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-04-21 17:48:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+
+max record is limited to 253 characters
+
+
+
+
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+domain=shubhamtatvamasi.com
+maxlength=253
+
+for i in {1..80}
+do
+   subdomain="${subdomain}${i}."
+done
+
+fulldomain=${subdomain}${domain}
+
+domainlength=$(echo $fulldomain | wc -c)
+
+if [ "$domainlength" -le "$maxlength" ]; then
+  echo "$fulldomain"
+else
+  echo "Your domain is too long. It can be max of $maxlength characters."
+fi
+
